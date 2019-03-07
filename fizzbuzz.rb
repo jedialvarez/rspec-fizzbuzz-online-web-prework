937f24b81a9adb506a782ecbdb41a2d9521cc2c8
Cody Alvarez <jedi.alvarez805@gmail.com>
2019-03-07 20:29:54 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,6 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisable by 3
     "Fizz"
-  end
-  if int % 5 == "buzz"
-    return
   end # Go fizz
 end
 # Don't forget! This file needs to be 'required' in its spec file
