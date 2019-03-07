0d327ad7a6e25c8a1b8418865c18b2908f834a81
Cody Alvarez <jedi.alvarez805@gmail.com>
2019-03-07 20:57:46 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,7 @@
 def fizzbuzz(int)
   if int % 3 == "Fizz"# if the number int is divisable by 3
   end
-  if int % 5 == 0
-    "Buzz"
+  if int % 5 == "Buzz"
   end
   if int % 15 == "Fizzbuzz"
   end
